0413b1ac4d4aec35947bfef8714025218bf53e5e
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2024-02-14 00:35:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(
     cookie:{
     sameSite : "none",
     secure: true,
-    domain: "myapp.vercel.app",
+    domain: "https://todo-app-vite-frontend.vercel.app",
     httpOnly: true
     }
   })
